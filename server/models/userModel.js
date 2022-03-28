25c3149164c4c75c0d0910738cedfad14d030aec
Ayush Sharma <ayush20892@gmail.com>
2022-03-28 22:58:49 +0530

    Database Model

---
+++ server/models/userModel.js
@@ -0,0 +1,110 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "Please enter a name."],
+    maxLength: [30, "Name should be under 30 char."],
+  },
+  email: {
+    type: String,
+    require: [true, "Please enter an email."],
+    validate: [validator.isEmail, "Please enter a valid email format."],
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: [true, "Please enter a password."],
+    minLength: [6, "Password should be atleast 6 char"],
+    select: false,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  forgotPasswordCode: String,
+  forgotPasswordExpiry: Date,
+  likedVideos: [
+    {
+      // video: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+        required: true,
+      // },
+    },
+  ],
+  watchLater: [
+    {
+      // video: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+        required: true,
+      // },
+    },
+  ],
+  history: [
+    {
+      // video: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+        required: true,
+      // },
+    },
+  ],
+  playlists: [
+    {
+      playlistName: {
+        type: String,
+        required: true,
+      },
+      playlistVideos: [
+        {
+          // video: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video",
+            required: true,
+          // },
+        },
+      ],
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.isPasswordValidated = async function (userSentPassword) {
+  return await bcrypt.compare(userSentPassword, this.password);
+};
+
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_expiresIn,
+  });
+};
+
+userSchema.methods.getForgotPasswordCode = function () {
+  const forgotCode = crypto.randomBytes(3).toString("hex");
+
+  this.forgotPasswordCode = crypto
+    .createHash("sha256")
+    .update(forgotCode)
+    .digest("hex");
+
+  this.forgotPasswordExpiry = Date.now() + 20 * 60 * 100;
+
+  return forgotCode;
+};
+
+module.exports = mongoose.model("User", userSchema);
---
+++ server/models/videoModel.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+
+const videoSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title of the Video Required."],
+    trim: true,
+    maxLength: [70, "Video Title should not be more than 50 character,"],
+  },
+  creator: {
+    type: String,
+    required: [true, "Name of the Video Creator Required."],
+    maxLength: [30, "Creator name should not be more than 30 character,"],
+  },
+  videoId: {
+    type: String,
+    required: [true, "VideoId of the video Required."],
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  views: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: String,
+    required: [true, "Category of the Video Required."],
+    enum: {
+      values: [
+        "All",
+        "Javascript",
+        "ComputerScience",
+        "Music",
+        "Education",
+        "Finance",
+        "Web3.0",
+        "Cryptocurrency",
+      ],
+      message: "Please select the category",
+    },
+  },
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Video", videoSchema);
